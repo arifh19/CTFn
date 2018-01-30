990e2ac5ccb35e4d1889f08d499feea80030c997
Fadli Maulana <wetmanz23@gmail.com>
2018-01-30 21:13:56 +0700

    Add delete contest method

--- CTFd/contests.py
+++ CTFd/contests.py
@@ -1,62 +1,91 @@
-from flask import render_template, request, redirect, jsonify, url_for, session, Blueprint
-from sqlalchemy.sql import or_, and_
+from flask import current_app as app, render_template, request, redirect, jsonify, url_for, Blueprint
+from CTFd.utils import admins_only, is_admin, cache
+from CTFd.models import db, Teams, Solves, Awards, Challenges, WrongKeys, Keys, Tags, Files, Tracking, Pages, Config, Hints, Unlocks, DatabaseError, Contests
+from CTFd.plugins.keys import get_key_class, KEY_CLASSES
+from CTFd.plugins.challenges import get_chal_class, CHALLENGE_CLASSES
 
-from CTFd.models import db, Challenges, Files, Solves, WrongKeys, Keys, Tags, Teams, Awards, Hints, Unlocks, Contests, Participations
 from CTFd import utils
 
-contests = Blueprint('contests', __name__)
+from datetime import datetime
+import pytz
+import os
 
-@contests.route('/contests', methods=['GET'])
-def contests_view():
-    contests = Contests.query.all()
-    contests_dict = []
+admin_contests = Blueprint('admin_contests', __name__)
 
-    for x in contests:
-        c = vars(x)
-        c.pop('_sa_instance_state', None)
-        c['is_active'] = x.is_active
-        contests_dict.append(c)
 
-    return render_template('contests.html', contests=contests_dict)
+@admin_contests.route('/admin/contests', methods=['POST', 'GET'])
+@admins_only
+def admin_contests_board():
+    if request.method == 'POST':
+        contests = Contests.query.order_by(Contests.id).all()
 
-@contests.route('/contest/<contest_slug>/participate/confirm', methods=['GET'])
-def contest_participate_confirm(contest_slug):
-    if utils.user_can_view_challenges():  # Do we allow unauthenticated users?
-        contest = Contests.query.filter_by(slug=contest_slug).first()
-        if utils.is_participated(contest):
-            return redirect(url_for("challenges_view_contest", contest_slug=contest_slug))
+        json_data = {'contests': []}
+        for x in contests:
+            contest_dict = vars(x)
+            contest_dict.pop('_sa_instance_state', None)
+            json_data['contests'].append(contest_dict)
 
-        if contest.protected:
-            if request.args.get('password') == contest.password:
-                participation = Participations(session.get('id'), contest.id)
-                db.session.add(participation)
-                db.session.commit()
-                db.session.close()
-                return redirect(url_for("challenges.challenges_view_contest", contest_slug=contest_slug))
-            else:
-                return redirect(url_for("contests.contest_participate", contest_slug=contest_slug))
-
-        else:
-            participation = Participations(session.get('id'), contest.id)
-            db.session.add(participation)
-            db.session.commit()
-            db.session.close()
-
-            return redirect(url_for("challenges.challenges_view_contest", contest_slug=contest_slug))
+        db.session.close()
+        return jsonify(json_data)
     else:
-        return redirect(url_for('auth.login', next='challenges'))
+        return render_template('admin/contests.html')
 
-@contests.route('/contest/<contest_slug>/participate', methods=['GET'])
-def contest_participate(contest_slug):
-    if utils.user_can_view_challenges():  # Do we allow unauthenticated users?
-        contest = Contests.query.filter_by(slug=contest_slug).first()
-        if utils.is_participated(contest):
-            return redirect(url_for("challenges.challenges_view_contest", contest_slug=contest_slug))
+@admin_contests.route('/admin/contest/new', methods=['POST', 'GET'])
+@admins_only
+def admin_create_contests():
+    if request.method == 'POST':
+        # Create contest
 
+        start_time = datetime.strptime(request.form['starttime'], '%Y-%m-%dT%H:%M')
+        end_time = datetime.strptime(request.form['starttime'], '%Y-%m-%dT%H:%M')
+
+        start_time = pytz.timezone(request.form['timezone']).localize(start_time).astimezone(pytz.utc)
+        end_time = pytz.timezone(request.form['timezone']).localize(end_time).astimezone(pytz.utc)
+
+        contest = Contests(request.form['slug'], request.form['name'], request.form['description'], start_time, end_time)
+
+        db.session.add(contest)
+        db.session.flush()
+        db.session.commit()
+        db.session.close()
+
+        return redirect(url_for('admin_contests.admin_contests_board'))
+    else:
+        return render_template('admin/contests/create.html')
+
+@admin_contests.route('/admin/contest/<int:contestid>', methods=['POST', 'GET'])
+@admins_only
+def admin_update_contests(contestid):
+    if request.method == 'POST':
+        tz = pytz.timezone(request.form['timezone'])
+
+        contest = Contests.query.filter_by(id=contestid).first_or_404()
+        contest.name = request.form['name']
+        contest.slug = request.form['slug']
+        contest.description = request.form['description']
+        contest.starttime = tz.localize(datetime.strptime(request.form['starttime'], '%Y-%m-%dT%H:%M')).astimezone(pytz.utc).replace(tzinfo=None)
+        contest.endtime = tz.localize(datetime.strptime(request.form['endtime'], '%Y-%m-%dT%H:%M')).astimezone(pytz.utc).replace(tzinfo=None)
+        contest.protected = True if request.form['protected'] == 'True' else False
+        contest.password = request.form['password']
+
+        db.session.add(contest)
+        db.session.commit()
+        db.session.close()
+
+        return redirect(url_for('admin_contests.admin_contests_board'))
+    else:
+        contest = Contests.query.filter_by(id=contestid).first()
         contest_dict = vars(contest)
         contest_dict.pop('_sa_instance_state', None)
+        return render_template('admin/contests/update.html', contest=contest_dict)
 
-        return render_template('participating.html', contest=contest_dict)
-    else:
-        return redirect(url_for('auth.login', next='challenges'))
 
+@admin_contests.route('/admin/contest/<int:contestid>/delete', methods=['POST'])
+@admins_only
+def admin_delete_contests(contestid):
+    if request.method == 'POST':
+        contest = Contests.query.filter_by(id=contestid).first_or_404()
+        db.session.delete(contest)
+        db.session.commit()
+        db.session.close()
+        return redirect(url_for('admin_contests.admin_contests_board'))
